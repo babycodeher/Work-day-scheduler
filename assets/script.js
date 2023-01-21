20947bbbff972e4750b4b599c13fae766fc15050
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-01-21 12:49:40 +0000

    added event listener to button

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,24 @@
 let currentDay = moment();
+let currentTime = moment();
+let time = $(".time");
+
+
 $("#currentDay").text(currentDay.format("dddd, MMMM do"));
+
+// $("currentTime").text(currentTime.format("HH a"))
+// localStorage.setItem();
+
+
+// This gives 
+$("button").on("click", function(event) {
+
+   console.log($(event.target).siblings("textarea").val());
+
+    console.log($(event.target).attr("data-time"));
+}) 
+
+// if (time === currentTime) {
+    
+// } else {
+    
+// }
