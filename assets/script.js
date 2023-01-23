10e59a02abf5d3b21f7b706ebb3296936ed90a74
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-01-24 00:46:52 +0200

    added timeblock in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
 let currentDay = moment();
 let currentTime = moment();
-let time = $(".time");
+let time = $(".row");
 
 
-$("#currentDay").text(currentDay.format("dddd, MMMM do"));
+$("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
-// $("currentTime").text(currentTime.format("HH a"))
+// $("currentTime").text(currentTime.format("hh a"))
 // localStorage.setItem();
 
 
 // This gives 
 $("button").on("click", function(event) {
-
-   console.log($(event.target).siblings("textarea").val());
-
+    
+    console.log($(event.target).siblings("textarea").val());
+    
     console.log($(event.target).attr("data-time"));
+    
+    event.preventDefault();
 }) 
 
-// if (time === currentTime) {
-    
-// } else {
-    
+// if (time === "9am") {
+//     $("row").css({"background-color": "#ff6961", "color": "white"});
 // }
