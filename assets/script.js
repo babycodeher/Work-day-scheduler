bd5f2638e757e027f89eb81f7076208dcf924307
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-01-24 02:58:26 +0200

    modified timeblock code in HTML

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,21 @@
 let currentDay = moment();
-let currentTime = moment();
-let time = $(".row");
+// let currentHour = moment().hours("ha");
 
 
+// Displays current day and date
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
-// $("currentTime").text(currentTime.format("hh a"))
+
 // localStorage.setItem();
 
 
 // This gives 
-$("button").on("click", function(event) {
+$(".saveBtn").on("click", function(event) {
     
+    // Check to confirm which timeblock button was clicked
     console.log($(event.target).siblings("textarea").val());
     
     console.log($(event.target).attr("data-time"));
     
-    event.preventDefault();
 }) 
 
-// if (time === "9am") {
-//     $("row").css({"background-color": "#ff6961", "color": "white"});
-// }
