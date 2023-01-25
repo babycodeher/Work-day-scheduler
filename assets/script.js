06c5698a7d8e08eb39b4402e0b92926f2f38f8ab
Wumi Afo <117525566+babycodeher@users.noreply.github.com>
2023-01-25 02:14:58 +0200

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,42 @@
 let currentDay = moment();
 let currentHour = moment().hours();
 
-
 // Displays current day and date
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
 
-// localStorage.setItem();
+// Specifies the style for the hour element by defining style as present, past or future
 
+$(".time-block").each(function(){
+    let hour = parseInt($(this).attr("id").split(["-"])[1]);
+console.log(hour);
+    if(hour === currentHour) {
+        $(this).addClass("present");
+    } else if(hour < currentHour) {
+        $(this).removeClass("present");
+        $(this).addClass("past");   
+        
+    } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");   
+    }
+});
 
-// This gives 
+// Event listener for when a button is clicked
 $(".saveBtn").on("click", function(event) {
     
-    // Check to confirm which timeblock button was clicked
-    console.log($(event.target).siblings("textarea").val());
+    // Specifies which time button is clicked and corresponding schedule
+    let scheduleItem = ($(event.target).siblings("textarea").val());
     
-    console.log($(event.target).attr("data-time"));
+    let dataTime = ($(event.target).attr("data-time"));
     
-}) 
+    // console.log(scheduleItem);
+    // console.log(dataTime);
 
-function hourEl(){
-    $(".time-block").each(function(){
-        let hour = parseInt($(this).attr("id").split("-")[1]);
-    
-        if(hour === currentHour) {
-            $(this).addClass("present");
-        } else if(hour < currentHour) {
-            $(this).removeClass("present");
-            $(this).addClass("past");   
-        } else if (hour > currentHour) {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");   
-        }
-    });
-    };
-    
-    hourEl()
+    // Save the data to local Storage
+    localStorage.setItem(dataTime, scheduleItem);
+}); 
+
+// This code saves user input in local storage
+// localStorage.setItem("");
